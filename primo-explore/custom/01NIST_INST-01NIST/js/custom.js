466f13933620458f7e7bc1861b35c2dfb25860bb
MDavisMCD <matthew.c.davis@nist.gov>
2024-02-15 11:14:33 -0500

    added report feature (live in sb)

--- primo-explore/custom/01NIST_INST-01NIST/js/custom.js
+++ primo-explore/custom/01NIST_INST-01NIST/js/custom.js
@@ -5,6 +5,37 @@
 var app = angular.module('viewCustom', ['angularLoad']);
 
 
+app.controller('someController', ['angularLoad', function (angularLoad) {
+    angularLoad.loadScript('/primo-explore/custom/01NIST_INST-01NIST/js/discovery-showcase.bundled.js');
+    }]);
+
+/*----------below is the code for Servicenow item level help link-----------*/
+
+       app.controller('prmActionContainerAfterController', [function () {
+        var vm = this;
+        
+        this.$onInit = function(){
+            {
+    
+        vm.getPermalink = getPermalink;
+    
+        function getPermalink() {
+            var permalink = encodeURIComponent(window.location.href);
+    
+            var formField = 'https://nist.servicenowservices.com/library?id=sc_cat_item&sys_id=529edfea1b795410348d9605bc4bcb66&referring_url=';
+            formField += permalink;
+           return formField;
+        }
+            }
+        };				
+    }]);
+    
+    app.component('prmActionContainerAfter', {
+        bindings: { parentCtrl: '<' },
+        controller: 'prmActionContainerAfterController',
+        template: '<div id="report-problem" layout="row" layout-align="center center"><a id="problemLink" target="_blank" href="{{$ctrl.getPermalink()}}" title="Report a problem"><img src="https://nist.primo.exlibrisgroup.com/discovery/custom/01NIST_INST_10NIST/img/warning.png">&nbsp;&nbsp;Report a problem with this item</a></div>'
+    });
+                /*----------Servicenow item level help link ends here-----------*/
 
 
 
