e24193d6c84e05f6a83e124eed9dc1b3a097fbd6
MDavisMCD <matthew.c.davis@nist.gov>
2024-07-26 11:41:30 -0400

    removes comment code

--- primo-explore/custom/01NIST_INST-01NIST/js/custom.js
+++ primo-explore/custom/01NIST_INST-01NIST/js/custom.js
@@ -213,20 +213,6 @@ app.controller("externalLinkController", [function ($stateParams, $state) {
     template: '<div id="ic-external-links"><h3 ng-class="section-title-header"><span>Try My Search In</span></h3><div id="ic-ebsco-link-block"><a href="{{$ctrl.chipsSearchUrl}}" target="_blank" id="ic-ebsco-link"><img src="custom/{{$ctrl.view}}/img/ebsco.svg" alt=""> {{$ctrl.chipsLabel}} <prm-icon svg-icon-set="primo-ui" icon-type="svg" icon-definition="open-in-new"></prm-icon></a></div><div id="ic-google-link-block"><a href="{{$ctrl.googleSearchUrl}}" target="_blank" id="ic-google-link"><img src="custom/{{$ctrl.view}}/img/google.svg" alt=""> {{$ctrl.googleLabel}} <prm-icon svg-icon-set="primo-ui" icon-type="svg" icon-definition="open-in-new"></prm-icon></a></div><div id="ic-worldcat-link-block"><a href="{{$ctrl.worldCatSearchUrl}}" target="_blank" id="ic-worldcat-link"><img src="custom/{{$ctrl.view}}/img/WorldCat.svg" alt=""> {{$ctrl.worldCatLabel}} <prm-icon svg-icon-set="primo-ui" icon-type="svg" icon-definition="open-in-new"></prm-icon></a></div></div>'
   });
 
-
-/* Try my search list of links to include */
-/*
-<li><a href="https://scholar.google.com/scholar?q={{$ctrl.getSearchTerm()}}" target="_blank">Google Scholar</a></li>\
-<li><a href="https://www.worldcat.org/search?qt=worldcat_org_all&q={{$ctrl.getSearchTerm()}}" target="_blank">Worldcat</a></li>\
-<li><a href="https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText={{$ctrl.getSearchTerm()}}" target="_blank">IEEE</a></li>\
-<li><a href="https://www.sciencedirect.com/search?qs={{$ctrl.getSearchTerm()}}" target="_blank">ScienceDirect</a></li>\
-<li><a href="https://pubmed.ncbi.nlm.nih.gov/?term={{$ctrl.getSearchTerm()}}" target="_blank">PubMed</a></li>\
-<li><a href="https://mathscinet.ams.org/mathscinet/publications-search?query={{$ctrl.getSearchTerm()}}&page=1&size=20&sort=newest&facets=" target="_blank">MathSciNet</a></li>\
-<li><a href="https://app.knovel.com/kn/search?include_synonyms=off&page=0&query={{$ctrl.getSearchTerm()}}" target="_blank">Knovel</a></li>\
-<li><a href="https://nist.primo.exlibrisgroup.com/discovery/search?query=any,contains,{{$ctrl.getSearchTerm()}}&tab=Everything&search_scope=MyInst_and_CI&vid=01NIST_INST:01NIST&offset=0&pcAvailability=true" target="_blank">Expanded Results</a></li>\
-<li><a href="https://inet.nist.gov/library/references?s={{$ctrl.getSearchTerm()}}&k=&a=&type=All&y=All&items_per_page=10" target="_blank">CHIPS Lit</a></li>\
-*/
-
 // No Results Page Changes
 
 app.controller('prmNoSearchResultAfterController', [function() {
