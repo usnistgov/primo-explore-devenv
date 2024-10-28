f6091707fe4b9c8914aba3b2ea8175808ac93a81
MDavisMCD <matthew.c.davis@nist.gov>
2024-10-28 12:18:22 -0400

    updates gtag code

--- primo-explore/custom/01NIST_INST-01NIST/js/custom.js
+++ primo-explore/custom/01NIST_INST-01NIST/js/custom.js
@@ -274,19 +274,26 @@ app.component('prmAuthoritySearchAfter', {
 });
 
 //START - Google Analytics
+//STANDARD CODE with edit to first line from 'async src' to 'googleAnalyticsUrl.src'
 
-var googleAnalyticsUrl = document.createElement('script');
-googleAnalyticsUrl.src = "https://www.googletagmanager.com/gtag/js?id=G-4KM704R98T";
-googleAnalyticsUrl.type = 'text/javascript';
-googleAnalyticsUrl.async = true;
-document.head.appendChild(googleAnalyticsUrl);
+googleAnalyticsUrl.src ="https://www.googletagmanager.com/gtag/js?id=G-4KM704R98T";
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());
+gtag('config', 'G-4KM704R98T');
 
-var googleAnalyticsCode = document.createElement('script');
-googleAnalyticsCode.innerHTML = `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-4KM704R98T');`;
-document.head.appendChild(googleAnalyticsCode);
+//OLD CODE
+//var googleAnalyticsUrl = document.createElement('script');
+//googleAnalyticsUrl.src = "https://www.googletagmanager.com/gtag/js?id=G-4KM704R98T";
+//googleAnalyticsUrl.type = 'text/javascript';
+//googleAnalyticsUrl.async = true;
+//document.head.appendChild(googleAnalyticsUrl);
+
+//var googleAnalyticsCode = document.createElement('script');
+//googleAnalyticsCode.innerHTML = `window.dataLayer = window.dataLayer || [];
+//function gtag(){dataLayer.push(arguments);}
+//gtag('js', new Date());
+//gtag('config', 'G-4KM704R98T');`;
+//document.head.appendChild(googleAnalyticsCode);
 
 //END - Google Analytics
 
